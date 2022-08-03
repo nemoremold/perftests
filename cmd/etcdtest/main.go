f7590961e0ee5be159031f2459b41942f5af83b5
i343811 <emoin.lanyu@sap.com>
2022-08-03 13:58:04 +0800

    Add test for etcd perf

---
+++ cmd/etcdtest/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/spf13/pflag"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"k8s.io/klog"
+)
+
+func main() {
+	var (
+		endpoints []string
+		testCount int
+	)
+
+	pflag.StringArrayVarP(&endpoints, "endpoints", "e", nil, "etcd endpoints")
+	pflag.IntVarP(&testCount, "test-count", "c", 100, "number of tests")
+
+	c, err := clientv3.New(clientv3.Config{
+		Endpoints: endpoints,
+	})
+	if err != nil {
+		klog.Fatal(err)
+	}
+
+	startTime := time.Now()
+	for i := 0; i < testCount; i++ {
+		for {
+			resp, err := c.Put(context.Background(), fmt.Sprintf("put-%v", i), fmt.Sprintf("%v-%v", i, time.Now().UnixMilli()))
+			if err == nil {
+				break
+			} else {
+				klog.Errorf("Failed to write put-%v", i)
+			}
+
+			klog.Info(resp)
+		}
+	}
+	duration := time.Since(startTime).Minutes()
+	putsPerSecond := float64(testCount) / duration
+
+	klog.Infof("Test duration:   %v", duration)
+	klog.Infof("Puts per second: %v", putsPerSecond)
+}
--- go.mod
+++ go.mod
@@ -7,9 +7,11 @@ require (
 	github.com/prometheus/client_golang v1.12.1
 	github.com/prometheus/client_model v0.2.0
 	github.com/spf13/pflag v1.0.5
+	go.etcd.io/etcd/client/v3 v3.5.1
 	k8s.io/api v0.24.0
 	k8s.io/apimachinery v0.24.0
 	k8s.io/client-go v0.24.0
+	k8s.io/klog v1.0.0
 	k8s.io/klog/v2 v2.60.1
 	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9
 	sigs.k8s.io/controller-runtime v0.12.0
